e6e367ea97f77ccf87d489f07cfbf6b1fb6bf7de
ramanParad <raman@paradigmintelligence.ai>
2025-01-05 17:11:54 +0200

    Changed redirect uri to be env variable

--- src/app/api/handle-auth/route.ts
+++ src/app/api/handle-auth/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: Request) {
         code,
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: `${process.env.NEXT_PUBLIC_APP_URL}/thank-you`,
+        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
         grant_type: 'authorization_code',
         access_type: 'offline',
       }),
